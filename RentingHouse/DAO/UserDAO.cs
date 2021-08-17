24320fa2641cf95025cd67b9bd6e10630a04a751
cloudlionheat <cloudlionheat@gmail.com>
2021-08-17 14:25:59 +0700

    init

---
+++ RentingHouse/DAO/UserDAO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RentingHouse.DTO;
+
+
+namespace RentingHouse.DAO
+{
+    // chứa các hàm xử lý của lớp
+    public class UserDAO
+    {
+        private static UserDAO instance;
+
+        public static UserDAO Instance //Phương thức singleton
+        {
+            get { if (instance == null) instance = new UserDAO(); return instance; } 
+            private set { instance = value; }
+        }
+
+        private UserDAO() { }
+
+        // Các hàm chức năng của lớp user
+
+        // Trong lớp DataProvider có chứa 3 phương thức chính: 
+        // 1 - ExecuteQuery : thực thi truy vấn có trả về dữ liệu (SELECT)
+        // 1 - ExecuteNonQuery : thực thi truy vấn trả dữ liệu 0-Fail,1-Success (INSERT, UPDATE, DELETE)
+        // 1 - ExecuteScalar  : thực thi truy vấn trả về là data của cột đầu tiên ở dòng đầu tiên (INSERT, UPDATE, DELETE) thường dùng để lấy id của dữ liệu sau truy vấn
+        public bool Login(string userName, string passWord)
+        {
+
+            string query = "USP_Login @userName , @passWord";// cấu truy vấn gọi procedure
+
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { userName, passWord });
+
+            return result.Rows.Count > 0;
+        }
+
+        public bool UpdateAccount(string userName, string fullName, string pass, string newPass)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("exec USP_UpdateAccount @userName , @displayName , @password , @newPassword", new object[] { userName, fullName, pass, newPass });
+
+            return result > 0;
+        }
+    }
+}
---
+++ RentingHouse/DTO/User.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingHouse.DTO
+{
+    //Lớp đối tượng khởi tạo định nghĩa lơp User (ko chứa các hàm xử lý chỉ để khởi tạo)
+    public class User
+    {
+        //Các phương thức khởi tạo
+        public User(string userName, string fullName, string dob, string phone, string idCard, string role, string password = null) //tham số passWord  khai báo optional, có gtri mặc định null nên để cuối hàng
+        {
+            this.UserName = userName;
+            this.FullName = fullName;
+            this.Dob = dob;
+            this.Phone = phone;
+            this.IdCard = idCard;
+            this.Role = role;
+            this.Password = password;
+        }
+
+        public User(DataRow row)
+        {
+            this.UserName = row["user_name"].ToString();
+            this.Password = row["password"].ToString();
+            this.FullName = row["full_name"].ToString();
+            this.Dob = row["dob"].ToString();
+            this.Phone = row["phone"].ToString();
+            this.IdCard = row["id_card"].ToString();
+            this.Role = row["role"].ToString();
+        }
+
+        // Khai báo thuộc tính
+        //Khai báo hết tất cả thuộc tính rồi CTRL+R+E nó tự động xuất ra các phương thức get set
+        private string userName;
+        private string password;
+        private string fullName;
+        private string dob;
+        private string phone;
+        private string idCard;
+        private string role;
+
+        //các phương thức get set
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+
+            set
+            {
+                userName = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+
+            set
+            {
+                fullName = value;
+            }
+        }
+
+        public string Dob
+        {
+            get
+            {
+                return dob;
+            }
+
+            set
+            {
+                dob = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public string IdCard
+        {
+            get
+            {
+                return idCard;
+            }
+
+            set
+            {
+                idCard = value;
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+
+            set
+            {
+                role = value;
+            }
+        }
+    }
+}
