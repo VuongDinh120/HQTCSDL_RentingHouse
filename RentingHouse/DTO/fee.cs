583874cc09d3d6a28d23c0aab5dfd8ed1bcf1bfc
cloudlionheat <cloudlionheat@gmail.com>
2021-08-21 18:46:00 +0700

    add fee class

---
+++ RentingHouse/DTO/fee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingHouse.DTO
+{
+    public class Fee
+    {
+
+        public Fee(string id, string name, float price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Fee(DataRow row)
+        {
+            this.Id = row["id"].ToString();
+            this.Name = row["name"].ToString();
+            this.Price = (float)row["price"];
+        }
+
+
+
+        // Khai báo thuộc tính
+        private string id;
+        private string name;
+        private float price;
+
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
